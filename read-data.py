7f05e6ba04bb89aba2632a9175b6a29ea834a10d
YuexiWangwei <wweihappy@126.com>
2018-06-11 14:58:55 +0800

    向read-data添加了输出数据与标签的类型与shape大小。

--- read-data.py
+++ read-data.py
@@ -28,8 +28,9 @@ def read_and_decode(filename):  # 读入dog_train.tfrecords
     # label = tf.cast(features['label'], tf.int32)  # 在流中抛出label张量，如果标签是单个的数字用此种方法
     return img, label
 
-image,label = read_and_decode("canjian-test-array.tfrecords")
-image_batch, label_batch = tf.train.shuffle_batch([image, label], batch_size=1, capacity=100, min_after_dequeue=20)
+image_test, label_test = read_and_decode("canjian-test-array.tfrecords")
+# image_batch, label_batch = tf.train.shuffle_batch([image, label], batch_size=1, capacity=100, min_after_dequeue=20)     #乱序取出相应图片
+image_test_batch, label_test_batch = tf.train.batch([image_test, label_test], batch_size=1300)
 
 with tf.Session() as sess: #开始一个会话
     coord=tf.train.Coordinator()
@@ -37,16 +38,25 @@ with tf.Session() as sess: #开始一个会话
 
     for i in range(1):
         # image_batch, label_batch = sess.run([image, label])  # 在会话中取出image和label
-        example, l = sess.run([image_batch, label_batch])#在会话中取出image和label
+        example, l = sess.run([image_test_batch, label_test_batch])#在会话中取出image和label
+
+        # # 以下输出数据的类型与大小------类型：numpy.ndarray   大小：（batch_size,224,224,3）
+        # print(type(example))
+        # print(example.shape)
+
+        # #以下输出标签的类型与大小------类型：numpy.ndarray   大小：（batch_size,5）
+        # print(type(l))
+        # print(l.shape)
+
         # #用于保存图片
         # img = Image.fromarray(example, 'RGB')  # 这里Image是之前提到的
         # example.save('_''Label_' + str(i) + '.jpg')  # 存下图片
-        # img = np.reshape(example,[224,224,3])
-        example = np.reshape(example, [224, 224, 3])
-        #用于显示图片
-        plt.imshow(example)
-        plt.show()
-        print(l)
+
+        # # 用于显示图片
+        # example = np.reshape(example, [224, 224, 3])
+        # plt.imshow(example)
+        # plt.show()
+        # print(l)
 
 
     coord.request_stop()
